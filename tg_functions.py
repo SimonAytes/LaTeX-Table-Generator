3cdd2464bdb9ef86084461a937b0a1b71eb4373c
Simon Aytes <simon@Simons-MacBook-Pro.local>
2021-04-27 16:55:41 -0400

    Setup initial folder structure

---
+++ tg_functions.py
@@ -0,0 +1,16 @@
+# Import csv
+def ImportCSV(filename = ""):
+    print("IMPORT CSV")
+
+
+# Get size of csv
+def GetSize(num_rows = 0):
+    print("GET SIZE")
+
+# Generate code holder
+def GenerateCodeHolder(contents = []):
+    print("GENERATE CODE HOLDER")
+
+# Generate table
+def GenerateTable(contents = []):
+    print("GENERATE TABLE")
